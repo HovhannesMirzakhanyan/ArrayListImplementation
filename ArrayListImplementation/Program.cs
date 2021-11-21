af6d08787ad198c66e29db3269e0f8ef1cfdb2c9
HovhannesMirzakhanyan <hovomirz@yahoo.com>
2021-11-21 13:31:12 +0400

    Implementing ArrayList

--- ArrayListImplementation/Program.cs
+++ ArrayListImplementation/Program.cs
@@ -10,6 +10,12 @@ namespace ArrayListImplementation
     {
         static void Main(string[] args)
         {
+            ArrayList arrList = new ArrayList();
+            arrList.Add(25);
+            arrList.Add("Monaco");
+            arrList.Add('Z');
+            arrList.Add(true);
+            arrList.Add(4.75);
         }
     }
 }
