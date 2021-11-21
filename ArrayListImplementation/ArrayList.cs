7e377d1aafdf7faca2c070e36025f892859ec9ff
HovhannesMirzakhanyan <hovomirz@yahoo.com>
2021-11-21 13:22:49 +0400

    Some Methods Added

--- ArrayListImplementation/ArrayList.cs
+++ ArrayListImplementation/ArrayList.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +16,13 @@ namespace ArrayListImplementation
         public ArrayList()
         {
             _items = emptyArray;
+        }
+        public virtual int Count
+        {
+            get
+            {
+                return _size;
+            }
         }
         public virtual int Capacity
         {
@@ -58,11 +65,25 @@ namespace ArrayListImplementation
         }
         public virtual int Add(Object value)
         {
-            
+
             if (_size == _items.Length) EnsureCapacity(_size + 1);
             _items[_size] = value;
             _version++;
             return _size++;
+        }
+        public virtual Object this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= _size) throw new ArgumentOutOfRangeException();
+                return _items[index];
+            }
+            set
+            {
+                if (index < 0 || index >= _size) throw new ArgumentOutOfRangeException();
+                _items[index] = value;
+                _version++;
+            }
         }
         private void EnsureCapacity(int min)
         {
@@ -74,5 +95,109 @@ namespace ArrayListImplementation
                 Capacity = newCapacity;
             }
         }
+        public virtual void CopyTo(Array array, int arrayIndex)
+        {
+            if ((array != null) && (array.Rank != 1))
+                throw new ArgumentException();
+            Array.Copy(_items, 0, array, arrayIndex, _size);
+        }
+        public virtual IEnumerator GetEnumerator()
+        {
+            return new ArrayListEnumeratorSimple(this);
+        }
+        private sealed class ArrayListEnumeratorSimple : IEnumerator, ICloneable
+        {
+            private ArrayList list;
+            private int index;
+            private int version;
+            private Object currentElement;
+            [NonSerialized]
+            private bool isArrayList;
+            static Object dummyObject = new Object();
+
+            internal ArrayListEnumeratorSimple(ArrayList list)
+            {
+                this.list = list;
+                this.index = -1;
+                version = list._version;
+                isArrayList = (list.GetType() == typeof(ArrayList));
+                currentElement = dummyObject;
+            }
+
+            public Object Clone()
+            {
+                return MemberwiseClone();
+            }
+
+            public bool MoveNext()
+            {
+                if (version != list._version)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                if (isArrayList)
+                {
+                    if (index < list._size - 1)
+                    {
+                        currentElement = list._items[++index];
+                        return true;
+                    }
+                    else
+                    {
+                        currentElement = dummyObject;
+                        index = list._size;
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (index < list.Count - 1)
+                    {
+                        currentElement = list[++index];
+                        return true;
+                    }
+                    else
+                    {
+                        index = list.Count;
+                        currentElement = dummyObject;
+                        return false;
+                    }
+                }
+            }
+
+            public Object Current
+            {
+                get
+                {
+                    object temp = currentElement;
+                    if (dummyObject == temp)
+                    {
+                        if (index == -1)
+                        {
+                            throw new InvalidOperationException();
+                        }
+                        else
+                        {
+                            throw new InvalidOperationException();
+                        }
+                    }
+
+                    return temp;
+                }
+            }
+
+            public void Reset()
+            {
+                if (version != list._version)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                currentElement = dummyObject;
+                index = -1;
+            }
+        }
+
     }
 }
